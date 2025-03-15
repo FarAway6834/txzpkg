941ada3723dfb0de80eda16e84a746a8c4f5a1fc
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-15 16:34:20 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='txzpkg',
-    version='0.0.2',
+    version='0.0.3',
     description='download pkg as txz, install as txz',
     author='du7ec',
     author_email='dutec6834@gmail.com',
